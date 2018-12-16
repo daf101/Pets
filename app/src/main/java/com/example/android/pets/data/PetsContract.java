ec58312a6af0dbc42de079fa8a73b78c021c3b10
daf101 <jonesmatt1991@gmail.com>
2018-12-16 16:06:54 +0800

    End of Lesson 2
    Finished Lesson 2, saving code should is be lost.

---
+++ app/src/main/java/com/example/android/pets/data/PetsContract.java
@@ -0,0 +1,25 @@
+package com.example.android.pets.data;
+
+import android.provider.BaseColumns;
+
+public final class PetsContract {
+
+    public static abstract class PetEntry implements BaseColumns {
+
+        public static final String TABLE_NAME = "pets";
+
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_PET_NAME = "name";
+        public static final String COLUMN_PET_BREED = "breed";
+        public static final String COLUMN_PET_GENDER = "gender";
+        public static final String COLUMN_PET_WEIGHT = "weight";
+
+        /**
+         * Possible values for the pet gender:
+         * */
+        public static final int GENDER_MALE = 1;
+        public static final int GENDER_FEMALE = 2;
+        public static final int GENDER_UNKNOWN = 0;
+
+    }
+}
